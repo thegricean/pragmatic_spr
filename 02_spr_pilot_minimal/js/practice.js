55c4d09965d676ab574c26245b119880374aba60
stela <stella.plamenova@gmail.com>
2017-05-18 11:00:14 +0200

    practice trials changed

--- 02_spr_pilot_minimal/js/practice.js
+++ 02_spr_pilot_minimal/js/practice.js
@@ -3,9 +3,9 @@ var practice = {};
 // creates the practice trial
 practice.init = function() {
 	var practice = [];
-	var quantifiers = ["All", "Some", "Some", "None", "Most"];
-	var colours = ["black", "black", "white", "black", "white"];
-	var blacks = [8, 3, 5, 0, 2];
+	var quantifiers = ["All", "Some", "None", "None"];
+	var colours = ["black", "white", "black", "white"];
+	var blacks = [10, 5, 0, 2];
 
 	for (var i = 0; i < quantifiers.length; i++) {
 		practice.push({
