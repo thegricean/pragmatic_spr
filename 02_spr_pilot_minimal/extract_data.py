9191de31ed19a427e3873200aa93426112d128ec
stela <stella.plamenova@gmail.com>
2017-06-07 17:19:33 +0200

    extact_data.py is now working properly

---
+++ 02_spr_pilot_minimal/extract_data.py
@@ -0,0 +1,139 @@
+import argparse
+import csv
+import json
+
+
+def main(raw_data):
+
+	responses, assignment_ids = prepare_data(raw_data)
+	create_csv(responses, assignment_ids)
+
+
+def prepare_data(raw_data):
+	"""
+	Takes mturk's file to prepare the data for csv
+	Returns a list with responses and a list with assignment ids
+	"""
+	responses = []
+
+	def get_data():
+		"""
+		Returns one list with the assignment ids and another one with the responses
+
+		"""
+		assignment_ids = []
+		raw_results = []
+		header = []
+		body = []
+
+		for line in raw_data:
+			if not header:
+				header = line.strip().split('\t')
+			else:
+				body.append(line.strip().split('\t'))
+
+		for hit in range(0, len(body)):
+			assignment_ids.append(body[hit][header.index("\"assignmentid\"")])
+			# raw_results is a list of strings
+			# each string contains the results of one assignment
+			raw_results.append(body[hit][header.index("\"Answer.results\"")])
+
+		return assignment_ids, raw_results
+
+	def remove_extra_quotes(li):
+		"""
+		Takes a list of string items and strips the extra quotes
+		Returns a list of valid jsons
+		"""
+		valid_jsons = []
+
+		for i in range(0, len(li)):
+			new_s = ""
+
+			for j in range(1, len(li[i])-1):
+				if li[i][j] == "\"" and li[i][j+1] == "\"":
+					continue
+				else:
+					new_s = new_s + li[i][j]
+
+			valid_jsons.append(new_s)
+
+		return valid_jsons
+
+	assignment_ids, raw_results = get_data()
+	results = remove_extra_quotes(raw_results)
+
+	for result in results:
+		responses.append(json.loads(result))
+
+	return responses, assignment_ids
+
+
+def create_csv(li, assignment_ids):
+	"""
+	Takes a list with data and a list with assignment ids
+	Creates a results.csv with the results
+	"""
+
+	with open('results.csv', 'w', encoding="utf-8") as f:
+		w = csv.writer(f)
+
+		"""
+		Headers
+		"""
+		w.writerow(['assignment_id', 'white_balls', 'black_balls', 'quantifier', 'colour',
+			'"all"', '"of"', '"the"', '"balls"', '"are"', 'target_colour', '"in"', '"the"', '"picture"',
+			'answer', 'answer_time',
+			'age', 'gender', 'languages', 'education', 'comments'])
+
+		# for each assignment
+		for assignment in range(0, len(li)):
+
+			# for each trial
+			for trial in li[assignment]['results']:
+				assignment_id = assignment_ids[assignment]
+				age = li[assignment]['subjectInfo']['age']
+				gender = li[assignment]['subjectInfo']['gender']
+				languages = li[assignment]['subjectInfo']['languages']
+				education = li[assignment]['subjectInfo']['education']
+				comments = li[assignment]['subjectInfo']['comments']
+
+				w.writerow([
+					assignment_id,
+					trial['all'] - trial['black'],
+					trial['black'],
+					trial['quantifier'],
+					trial['colour'],
+					trial['readingTimes'][0],
+					trial['readingTimes'][1],
+					trial['readingTimes'][2],
+					trial['readingTimes'][3],
+					trial['readingTimes'][4],
+					trial['readingTimes'][5],
+					trial['readingTimes'][6],
+					trial['readingTimes'][7],
+					trial['readingTimes'][8],
+					trial['response'][0],
+					trial['response'][1],
+					age,
+					gender,
+					languages,
+					education,
+					comments
+					])
+
+"""
+The cli
+"""
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description=(
+		'turns mturk\'s data into csv'
+		))
+
+	parser.add_argument('file',
+		help = 'path to mturk\'s output file',
+		type = argparse.FileType('r', encoding="utf-8"))
+
+	args = parser.parse_args()
+
+	main(args.file)
